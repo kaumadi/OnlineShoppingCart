be177a7239bf6097b5239f9686380479f7272501
Kaumadi Marasinghe <7821403+kaumadi@users.noreply.github.com>
2020-06-30 16:13:01 +0530

    Xunit test project added

---
+++ OnlineShoppingCart/XUnitTestOnlineShoppingCart/ControllerTest.cs
@@ -0,0 +1,42 @@
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using OnlineShoppingCart.BusinessLayer.IRepositories;
+using OnlineShoppingCart.Controllers;
+using OnlineShoppingCart.DataAccessLayer.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XUnitTestOnlineShoppingCart
+{
+    public class ControllerTest
+    {
+        [Fact]
+        public async Task Get_All_Products_Count_Check_Using_Moq()
+        {
+            // Arrange
+            var serviceMock = new Mock<IProductRepository<Product>>();
+
+            IEnumerable<Product> plist = new List<Product> { };
+            serviceMock.Setup(x => x.GetAllProductsAsync()).ReturnsAsync(() => new List<Product> {
+
+             new Product{ProductId=1, ProductName="Cake", UnitPrice=1250,UnitsInStock=2,Description="Item 001",ImagePath="1.png"},
+             new Product{ProductId=2, ProductName="Flowers", UnitPrice=1250,UnitsInStock=2,Description="Item 002",ImagePath="1.png"},
+             new Product{ProductId=3, ProductName="Soft Toys", UnitPrice=1250,UnitsInStock=2,Description="Item 003",ImagePath="1.png"} });
+            
+            var controller = new ProductController (serviceMock.Object);
+
+            // Act
+            var result = await controller.GetAllProductsAsync();
+
+            // Assert
+            var okResult = result.Should().BeOfType<OkObjectResult>().Subject;
+            var persons = okResult.Value.Should().BeAssignableTo<IEnumerable<Product>>().Subject;
+
+            persons.Count().Should().Be(3);
+        }
+    }
+    
+}
---
+++ OnlineShoppingCart/XUnitTestProject-OnlineShoppingCart/ControllerTest/ProductListControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using OnlineShoppingCart.BusinessLayer.IRepositories;
+using OnlineShoppingCart.DataAccessLayer.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XUnitTestProject_OnlineShoppingCart.ControllerTest
+{
+ 
+    [Fact]
+    public async Task ProductsGetFromMoq()
+    {
+        // Arrange
+        var serviceMock = new Mock<IProductRepository<Product>>();
+        serviceMock.Setup(x => x.GetAllProductsAsync()).Returns(() => new List<Product>
+  {
+    new Product{ProductId=1,ProductName="Cake",UnitPrice=2000,UnitsInStock=2, Description="", ImagePath=""},
+    new Product{ProductId=2,ProductName="Cake",UnitPrice=2000,UnitsInStock=2, Description="", ImagePath=""},
+     new Product{ProductId=3,ProductName="Cake",UnitPrice=2000,UnitsInStock=2, Description="", ImagePath=""}
+  }) ;
+        var controller = new ProductController(serviceMock.Object);
+
+        // Act
+        var result = await controller.Get();
+
+        // Assert
+        var okResult = result.Should().BeOfType<OkObjectResult>().Subject;
+        var persons = okResult.Value.Should().BeAssignableTo<IEnumerable<Person>>().Subject;
+
+        persons.Count().Should().Be(3);
+    }
+}
